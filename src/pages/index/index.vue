d456aaa468b6deb1af70024e595ba5bc69ffca8c
skyh24 <skyhighfeng@gmail.com>
2019-03-11 17:15:46 +0800

    code

---
+++ src/pages/index/index.vue
@@ -0,0 +1,37 @@
+<template>
+  
+</template>
+
+<script>
+import card from '@/components/card'
+
+export default {
+  data () {
+    return {
+      
+    }
+  },
+
+  components: {
+  },
+
+  methods: {
+    bindViewTap () {
+      const url = '../logs/main'
+      if (mpvuePlatform === 'wx') {
+        mpvue.switchTab({ url })
+      } else {
+        mpvue.navigateTo({ url })
+      }
+    }
+  },
+
+  created () {
+    // let app = getApp()
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
